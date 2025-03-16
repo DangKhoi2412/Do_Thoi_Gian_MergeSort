b8595b6794e79a1780aad39287e1b9038ffa2cce
khoi <khoi@DESKTOP-QTHNFM9>
2025-03-16 10:03:51 +0700

    Add project files.

---
+++ ConsoleApp27/DoThoiGian.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class DoThoiGian
+    {
+        class program
+        {
+            static void Main()
+            {
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
+                Console.WriteLine("Đang đo thời gian thuật toán Merge Sort...");
+                tinh.tinhthoigiantb("Dễ nhất (mảng đã sắp xếp)", taomang.denhat);
+                tinh.tinhthoigiantb("Trung bình (mảng ngẫu nhiên)", taomang.trungbinh);
+                tinh.tinhthoigiantb("Khó nhất (mảng sắp xếp ngược)", taomang.khonhat);
+                Console.ReadKey();
+            }
+        }
+        class taomang
+        {
+            static public int[] denhat(int size)
+            {
+                Random rand = new Random();
+                int[] arr = new int[size];
+                for (int i = 0; i < size; i++)
+                    arr[i] = rand.Next(1, size + 1);
+                Array.Sort(arr);
+                return arr;
+            }
+            static public int[] trungbinh(int size)
+            {
+                Random rand = new Random();
+                int[] arr = new int[size];
+                for (int i = 0; i < size; i++)
+                    arr[i] = rand.Next(1, size + 1);
+                return arr;
+            }
+
+            static public int[] khonhat(int size)
+            {
+                Random rand = new Random();
+                int[] arr = new int[size];
+                for (int i = 0; i < size; i++)
+                    arr[i] = rand.Next(1, size + 1);
+                Array.Sort(arr);
+                Array.Reverse(arr);
+                return arr;
+            }
+        }
+        class tinh
+        {
+            static public void tinhthoigiantb(string casename, Func<int, int[]> taomang)
+            {
+                Timing tg = new Timing();
+                TimeSpan tongtg = new TimeSpan(0);
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    int[] arr = taomang(10000);
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    tg.startTime();
+                    Program.mergeSort(arr, 0, arr.Length - 1);
+                    tg.StopTime();
+
+                    tongtg += tg.Result();
+                }
+
+                Console.WriteLine($"{casename}: Trung bình {tongtg.TotalMilliseconds / 1000} ms");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        public static void merge(int[] a, int l, int m, int r)
+        {
+            int n1 = m - l + 1, n2 = r - m;
+            int[] x = new int[n1];
+            int[] y = new int[n2];
+
+            Array.Copy(a, l, x, 0, n1);
+            Array.Copy(a, m + 1, y, 0, n2);
+
+            int i = 0, j = 0;
+
+            while (i < n1 && j < n2)
+            {
+                if (x[i] <= y[j])
+                {
+                    a[l] = x[i]; l++; i++;
+                }
+                else
+                {
+                    a[l] = y[j]; l++; j++;
+                }
+            }
+            while (i < n1)
+            {
+                a[l] = x[i]; l++; i++;
+
+            }
+            while (j < n2)
+            {
+                a[l] = y[j]; l++; j++;
+            }
+        }
+        public static void mergeSort(int[] a, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+                mergeSort(a, l, m);
+                mergeSort(a, m + 1, r);
+                merge(a, l, m, r);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = random.Next(1, 100);
+            }
+            mergeSort(a, 0, n - 1);
+
+            foreach (int x in a)
+            {
+                Console.Write(x + " ");
+            }
+        }
+            //Timing time = new Timing();
+            //TimeSpan tongtg = new TimeSpan(0);
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    int[] a = new int[n];
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        a[j] = random.Next(1, 1000);
+            //    }
+            //    //Array.Sort(a);
+            //    //Array.Reverse(a);
+            //    time.startTime();
+            //    mergeSort(a, 0, n - 1);
+            //    time.StopTime();
+
+            //    tongtg += time.Result();
+            //}
+            //Console.WriteLine(tongtg.TotalMilliseconds / 1000);
+        
+
+    }
+}
